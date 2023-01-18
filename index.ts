ee3a85760527ec42bde6e9b99b2b77a774c6c66a
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2023-01-18 22:38:09 +0100

    Deploy HTML file

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as pulumi from "@pulumi/pulumi";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("pulumi-test", {
+  bucket: "zvonimirr-pulumi-s3-test",
+  website: {
+    indexDocument: "index.html",
+  },
+});
+
+// Upload index.html to the bucket
+new aws.s3.BucketObject("index.html", {
+  bucket,
+  acl: "public-read",
+  contentType: "text/html",
+  source: new pulumi.asset.FileAsset("index.html"),
+});
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+// Export URL
+export const bucketUrl = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
